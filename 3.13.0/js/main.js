21d4145bb28e7e3d523a28445b505167c7f23285
orsolyalukacs <orchie.lucas@gmail.com>
2020-05-26 13:57:35 -0500

    Add coffee bar chart visualization.

--- 3.13.0/js/main.js
+++ 3.13.0/js/main.js
@@ -1,12 +1,16 @@
 /*
  *    main.js
  *    Mastering Data Visualization with D3.js
- *    Project 1 - Star Break Coffee
+ *    Project 1 - D3 Transitions and Update pattern
  */
 
 var margin = { left: 100, right: 10, top: 10, bottom: 150 };
 var width = 600 - margin.left - margin.right,
   height = 400 - margin.top - margin.bottom;
+
+var flag = true;
+
+var t = d3.transition().duration(750);
 
 // check if svg already exists to avoid webpack double-rendering
 if (!document.getElementsByTagName("svg").length) {
@@ -18,6 +22,19 @@ if (!document.getElementsByTagName("svg").length) {
     .append("g")
     .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
+  // x-axis: band scale
+  var x = d3.scaleBand().range([0, width]).paddingInner(0.3).paddingOuter(0.3);
+
+  // y-axis: linear scale
+  var y = d3.scaleLinear().range([height, 0]);
+
+  var xAxisGroup = g
+    .append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0, " + height + ")");
+
+  var yAxisGroup = g.append("g").attr("class", "y-axis");
+
   // X Label
   g.append("text")
     .attr("class", "x axis-label")
@@ -28,7 +45,8 @@ if (!document.getElementsByTagName("svg").length) {
     .text("Month");
 
   // Y Label
-  g.append("text")
+  var yLabel = g
+    .append("text")
     .attr("class", "y axis-label")
     .attr("x", -(height / 2))
     .attr("y", -60)
@@ -41,64 +59,99 @@ if (!document.getElementsByTagName("svg").length) {
     .then((data) => {
       data.forEach((d) => {
         d.revenue = +d.revenue;
-        console.log(d);
+        d.profit = +d.profit;
       });
 
-      // x-axis: band scale
-      var x = d3
-        .scaleBand()
-        .domain(
-          data.map((d) => {
-            return d.month;
-          })
-        )
-        .range([0, width])
-        .paddingInner(0.3)
-        .paddingOuter(0.3);
+      // call the update in the interval loop to upate every 1 sec
+      d3.interval(() => {
+        var newData = flag ? data : data.slice(1)
+        update(newData);
+        flag = !flag;
+      }, 1000);
 
-      var xAxisCall = d3.axisBottom(x);
-      g.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0, " + height + ")")
-        .call(xAxisCall);
-
-      // y-axis: linear scale
-      var y = d3
-        .scaleLinear()
-        .domain([
-          0,
-          d3.max(data, (d) => {
-            return d.revenue;
-          }),
-        ])
-        .range([height, 0]);
-
-      // display the revenue amounts on the y-axis
-      var yAxisCall = d3.axisLeft(y).tickFormat((d) => {
-        return "$" + d;
-      });
-
-      g.append("g").attr("class", "y-axis").call(yAxisCall);
-
-      // add a rectangle for each month of data that we have.
-      var rects = g.selectAll("rect").data(data);
-
-      rects
-        .enter()
-        .append("rect")
-        .attr("y", function (d) {
-          return y(d.revenue);
-        })
-        .attr("x", function (d) {
-          return x(d.month);
-        })
-        .attr("width", x.bandwidth)
-        .attr("height", function (d) {
-          return height - y(d.revenue);
-        })
-        .attr("fill", "grey");
+      // run the visualization for the first time
+      update(data);
     })
     .catch((error) => {
       console.log(error);
     });
+}
+
+// update:
+// 1. scales and axes in case the data changes
+// 2. size/position of the rectangles
+// what should actually change with the scales is the domain - it relies on the data.
+function update(data) {
+  // show revenue if flag is true, else show profit
+  var value = flag ? "revenue" : "profit";
+  //set the x axis domain
+  x.domain(
+    data.map((d) => {
+      return d.month;
+    })
+  );
+
+  // set the y axis domain
+  y.domain([
+    0,
+    d3.max(data, (d) => {
+      return d[value];
+    }),
+  ]);
+
+  // x axis
+  var xAxisCall = d3.axisBottom(x);
+  xAxisGroup.call(xAxisCall);
+
+  // y axis
+  // display the revenue amounts on the y-axis
+  var yAxisCall = d3.axisLeft(y).tickFormat((d) => {
+    return "$" + d;
+  });
+  yAxisGroup.transition(t).call(yAxisCall);
+
+  // JOIN new data with old elements.
+  // add a rectangle for each month of data that we have.
+  var rects = g.selectAll("rect").data(data,(d) => {
+    return d.month;
+  });
+
+  // EXIT old elements not present in new data.
+  rects
+    .exit()
+    .attr("fill", "red")
+    .transition(t)
+    .attr("y", y(0))
+    .attr("height", 0)
+    .remove();
+
+  // ENTER new elements present in new data
+  rects
+    .enter()
+    .append("rect")
+    .attr("y", y(0))
+   
+    .attr("y", (d) => {
+      return y(d[value]);
+    })
+    .attr("fill", "grey")
+
+    // UPDATE old elements present in new data
+    // before the merge function: we  only set attributes for enter selection
+    // after the merge function: we set attributes for both enter and update selection
+    .merge(rects)
+    .transition(t)
+    .attr("x", (d) => {
+      return x(d.month);
+    })
+    .attr("width", x.bandwidth)
+    .attr("y", (d) => {
+      return y(d[value]);
+    })
+    .attr("height", (d) => {
+      return height - y(d[value]);
+    })
+
+  var label = flag ? "Revenue" : "Profit";
+  yLabel.text(label);
 }
